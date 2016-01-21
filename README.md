06aa74b17b06e8464cdbee418c59adaedc1117a9
kingdido999 <kingdido999@gmail.com>
2016-01-20 21:10:23 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# moe.fm API wrapper (萌否电台)
+
+This is a node module that allows you to communicate with [萌否API](http://open.moefou.org/docs/).
---
+++ test/moefmTest.js
@@ -0,0 +1,121 @@
+const fs = require('fs');
+const path = require('path');
+const config = JSON.parse(fs.readFileSync(path.normalize(__dirname + '/config.json', 'utf8')));
+
+describe('Moefm API', function() {
+  const Moefm = require('../lib/moefm.js');
+  const API = new Moefm(config.apiKey);
+
+  // 条目 ----------------------------------------------------------------------
+  describe('wikis test', function() {
+    it('should get wikis of music and radio', function(done) {
+      API.wikis('music,radio', {}, done);
+    });
+  });
+
+  describe('onAir test', function() {
+    it('should get all on-air animes', function(done) {
+      API.onAir({}, done);
+    });
+  });
+
+  describe('wiki detail test', function() {
+    it('should get details of a wiki', function(done) {
+      API.wikiDetail('music', { wiki_id: 47251 }, done);
+    });
+  });
+
+  describe('wiki subs test', function() {
+    it('should get subs of a wiki', function(done) {
+      API.wikiSubs('music', { wiki_id: 47251 }, done);
+    });
+  });
+
+  describe('wiki favs test', function() {
+    it('should get favorites of a wiki', function(done) {
+      API.wikiFavs('music', { wiki_id: 47251 }, done);
+    });
+  });
+
+  describe('wiki ratings test', function() {
+    it('should get ratings of a wiki', function(done) {
+      API.wikiRatings('music', { wiki_id: 47251 }, done);
+    });
+  });
+
+  describe('wiki relationships test', function() {
+    it('should get relationships of a wiki', function(done) {
+      API.wikiRelationships('music', { wiki_id: 47251 }, done);
+    });
+  });
+
+  describe('wiki similarity test', function() {
+    it('should get similar wikis of a wiki', function(done) {
+      API.wikiSimilarity('music', { wiki_id: 47251 }, done);
+    });
+  });
+
+  // 子条目 --------------------------------------------------------------------
+  describe('sub detail test', function() {
+    it('should get details of a sub', function(done) {
+      API.subDetail('song', 218878, done);
+    });
+  });
+
+  describe('sub favorites test', function() {
+    it('should get favorites of a sub', function(done) {
+      API.subFavs('song', 218878, {}, done);
+    });
+  });
+
+  describe('sub ratings test', function() {
+    it('should get ratings of a sub', function(done) {
+      API.subRatings('song', 218878, {}, done);
+    });
+  });
+
+  describe('sub relationships test', function() {
+    it('should get relationships of a sub', function(done) {
+      API.subRelationships('song', 218878, {}, done);
+    });
+  });
+
+  // 用户 ----------------------------------------------------------------------
+  describe('userDetail test', function() {
+    it('should get user details', function(done) {
+      API.userDetail({ uid: 2127 }, done);
+    });
+  });
+
+  describe('userFavsWiki test', function() {
+    it('should get user favorite wikis', function(done) {
+      API.userFavsWiki({ uid: 2127 }, done);
+    });
+  });
+
+  describe('userFavsSub test', function() {
+    it('should get user favorite sub wikis', function(done) {
+      API.userFavsSub({ uid: 2127 }, done);
+    });
+  });
+
+  // 搜索 ----------------------------------------------------------------------
+  describe('searchWiki test', function() {
+    it('should get search result of a wiki', function(done) {
+      API.searchWiki('东方', {}, done);
+    });
+  });
+
+  describe('searchSub test', function() {
+    it('should get search result of a sub', function(done) {
+      API.searchSub('研究所', { sub_type: 'song' }, done);
+    });
+  });
+
+  // 播放接口 -------------------------------------------------------------------
+  describe('listenPlaylist test', function() {
+    it('should get a music list', function(done) {
+      API.listenPlaylist({}, done);
+    });
+  });
+});
